46c6b067c7ad9a9ba1a78f9cfd568e90c42ceaab
@kabongo6 <kabongo.nahounou@capgemini.com>
2024-06-28 15:14:21 -0500

    updated branches

--- .github/workflows/file1.sh
+++ .github/workflows/file1.sh
@@ -1,7 +1,9 @@
-echo $bash
-echo $bash_version
-echo $home
-echo $pwd
+echo our shell name is $bash
+echo our shell version is $bash_version
+echo our home directory is $home
+echo our current working directory is $pwd
 
 name=Francis
+val=18
 echo My best friend is $name
+echo the value is $val
